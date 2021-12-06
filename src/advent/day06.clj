e8c5f210feff44b24ec9a42baa8d0929741d79de
Aleh Suprunovich <br@ahlamon.org>
2021-12-06 20:23:44 +0300

    Invert for readability

--- src/advent/day06.clj
+++ src/advent/day06.clj
@@ -20,7 +20,7 @@
   (let [lanternfish (->> (string/split (slurp "inputs/day06") #",")
                          (map read-string)
                          frequencies)
-        after-80-days (last (take 81 (iterate day lanternfish)))
-        after-256-days (last (take 257 (iterate day lanternfish)))]
+        after-80-days (first (drop 80 (iterate day lanternfish)))
+        after-256-days (first (drop 256 (iterate day lanternfish)))]
     (println (reduce + 0 (vals after-80-days)) "lanternfish after 80 days")
     (println (reduce + 0 (vals after-256-days)) "lanternfish after 256 days")))

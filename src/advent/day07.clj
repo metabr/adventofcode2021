eb9c4a574e9c41808138771fb3f0e90f89427afe
Aleh Suprunovich <br@ahlamon.org>
2021-12-07 10:05:03 +0300

    Day 7

---
+++ src/advent/day07.clj
@@ -0,0 +1,32 @@
+(ns advent.day07
+  (:require
+   [clojure.string :as string]))
+
+(defn fuel [crabs pos]
+  (let [crab-fuel (fn [crab] (Math/abs (- crab pos)))]
+    (->>
+     (map crab-fuel crabs)
+     (reduce + 0))))
+
+(defn fuel2 [crabs pos]
+  (let [crab-distance (fn [crab] (Math/abs (- crab pos)))
+        crab-fuel (fn [crab] (apply + (range (inc (crab-distance crab)))))]
+    (->>
+     (map crab-fuel crabs)
+     (reduce + 0))))
+
+(defn run [& args]
+  (let [crabs (->> (string/split (slurp "inputs/day07") #",")
+                   (map read-string))
+        min-pos (apply min crabs)
+        max-pos (apply max crabs)]
+    (println "Part 1 [position fuel]:"
+             (->> (range min-pos (inc max-pos))
+                  (map-indexed (fn [i pos] [i (fuel crabs pos)]))
+                  (sort-by second)
+                  first))
+    (println "Part 2 [position fuel]:"
+             (->> (range min-pos (inc max-pos))
+                  (map-indexed (fn [i pos] [i (fuel2 crabs pos)]))
+                  (sort-by second)
+                  first))))

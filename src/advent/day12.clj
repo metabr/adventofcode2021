8717537fe02f36c09ed59a083995873cb4e4f140
Aleh Suprunovich <br@ahlamon.org>
2021-12-14 16:59:09 +0300

    Day 12, part 2

--- src/advent/day12.clj
+++ src/advent/day12.clj
@@ -21,18 +21,51 @@
 (defn findpath [graph start goal]
   ((dfs graph goal) [start] #{start}))
 
+(defn dfs-2 [graph goal]
+  (fn search [path visited twice]
+    (let [current (peek path)]
+      (if (= goal current)
+        [path]
+        (->> current
+             (get graph)
+             (remove visited)
+             (mapcat #(search (conj path %)
+                              (if (or (Character/isUpperCase (first %))
+                                      (contains? twice %))
+                                visited
+                                (conj visited %))
+                              (disj twice %))))))))
+
+(defn findpath-2 [graph start goal]
+  (let [small-caves (->> (mapcat val graph)
+                         distinct
+                         (filter #(not (= % "start")))
+                         (filter #(not (= % "end")))
+                         (filter #(Character/isLowerCase (first %))))]
+    (mapcat #((dfs-2 graph goal) [start] #{start} #{%}) small-caves)))
+
 (defn run [& _]
-  (let [input (->> (string/split (slurp "inputs/day12") #"\n")
-                   (map #(string/split % #"-"))
-                   (reduce (fn [m [k v]] (let [m1 (if (contains? m k)
-                                                    (update m k #(conj % v))
-                                                    (assoc m k [v]))
-                                               m2 (if (contains? m1 v)
-                                                    (update m1 v #(conj % k))
-                                                    (assoc m1 v [k]))]
-                                           m2))
-                           {}))]
-    (->>
-     (findpath input "start" "end")
-     count
-     (println "Part 1:"))))
+  (let [input
+        (->> (string/split (slurp "inputs/day12") #"\n")
+             (map #(string/split % #"-"))
+             (reduce
+              (fn [m [k v]]
+                (let [m1 (if (contains? m k)
+                           (update m k #(conj % v))
+                           (assoc m k [v]))
+                      m2 (if (contains? m1 v)
+                           (update m1 v #(conj % k))
+                           (assoc m1 v [k]))]
+                  m2))
+              {}))]
+     (time
+      (->>
+       (findpath input "start" "end")
+       count
+       (println "Part 1:")))
+     (time
+      (->>
+       (findpath-2 input "start" "end")
+       distinct
+       count
+       (println "Part 2:")))))

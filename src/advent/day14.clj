3f46672602bfa7951e704e5846bc96caf2d8c87d
Aleh Suprunovich <br@ahlamon.org>
2021-12-14 23:19:50 +0300

    Day 14, part 2

--- src/advent/day14.clj
+++ src/advent/day14.clj
@@ -2,47 +2,52 @@
   (:require
    [clojure.string :as string]))
 
-(defn polymerize [rules template]
-  (let [template (into [] template)
-        f (vector (first template))
-        r (pop (into [] (rest template)))
-        l (vector (peek template))]
-    (->>
-     (concat
-      f
-      (mapcat #(vector % %) r)
-      l)
-     (partition 2)
-     (map #(apply str %))
-     (map (fn [k] [(get rules k) (second k)]))
-     (apply (partial concat f))
-     (apply str))))
-
 (defn most-common-minus-least-common [polymer]
-  (->> polymer frequencies vals
+  (->> polymer
+       (map #(vector (first (first %)) (second %)))
+       (reduce (fn [m [k v]]
+                 (if (contains? m k)
+                   (update m k + v)
+                   (conj m [k v])))
+               {})
+       vals
        (#(let [max (apply max %)
                min (apply min %)]
            (- max min)))))
 
-(defn count-elements
-  "Return difference of most- and least-common elements."
-  [polymer]
-  (->> polymer
-       (map (fn [[k v]] {(first k) v}))   ; Can't use `update-keys` because
-       (apply merge-with +)               ; collisions; could use `reduce-kv`.
-       vals
-       (apply (juxt max min))
-       (apply -)))
+(defn insert [rules init]
+  (fn [polymer]
+    (->> polymer
+         (mapcat (fn [[pair count]]
+                   (map #(hash-map % count) (rules pair))))
+         (apply merge-with + init))))
+
+(defn apply-steps [steps template rules init]
+  (->> template
+       (iterate (insert rules init))
+       (drop steps)
+       first))
 
 (defn run [& _]
-  (let [input (string/split-lines (slurp "inputs/day14"))
-        polymer-template (first input)
-        pair-insertion-rules
-        (->> (drop 2 input)
-             (map #(string/split % #" -> "))
-             (mapcat #(vector (first %) (first (second %))))
-             (apply hash-map))
-        polymerize (partial polymerize pair-insertion-rules)
-        polymer (iterate polymerize polymer-template)]
-    (->> (drop 10 polymer) first most-common-minus-least-common
-         (println "After 10 steps: most common quantity - least common quantity ="))))
+  (let [[polymer-template pair-insertion-rules]
+        (string/split (slurp "inputs/day14") #"\n\n")
+        last-element (last polymer-template)
+        polymer-template (->> polymer-template
+                              (partition 2 1)
+                              frequencies)
+        pair-insertion-rules (->> (string/split-lines pair-insertion-rules)
+                                  (map #(string/split % #" -> "))
+                                  (map (fn [[[l r] [m]]] {[l r] [[l m] [m r]]}))
+                                  (into {}))
+        polymerize (fn [steps] (apply-steps steps
+                                            polymer-template
+                                            pair-insertion-rules
+                                            {[last-element] 1}))]
+
+    (->> (polymerize 10)
+         most-common-minus-least-common
+         (println "After 10 steps: most common quantity - least common quantity ="))
+
+    (->> (polymerize 40)
+         most-common-minus-least-common
+         (println "After 40 steps: most common quantity - least common quantity ="))))
